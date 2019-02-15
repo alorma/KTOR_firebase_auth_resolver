115d1323d73a3a5e69682fbd4207bfcc4c88401e
Bernat Borrás Paronella <bernat.borras@privalia.com>
2019-02-15 13:46:36 +0100

    Working

--- src/com/alorma/ktor/firebase/firebase.kt
+++ src/com/alorma/ktor/firebase/firebase.kt
@@ -1,5 +1,6 @@
 package com.alorma.ktor.firebase
 
+import com.google.gson.Gson
 import io.ktor.application.call
 import io.ktor.auth.Authentication
 import io.ktor.auth.AuthenticationPipeline
@@ -8,19 +9,18 @@ import io.ktor.auth.Principal
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.request.headers
-import io.ktor.client.request.url
 import io.ktor.util.flattenForEach
 import java.net.URL
 
-fun Authentication.Configuration.firebase(
+fun Authentication.Configuration.firebaseAuth(
     httpClient: HttpClient,
-    authUrl: String
+    auth: String
 ) {
     val provider = AuthenticationProvider(null)
 
     provider.pipeline.intercept(AuthenticationPipeline.RequestAuthentication) { context ->
-        val response = httpClient.get<AuthResponse> {
-            url(URL(authUrl.plus("authenticate")))
+        val authUrl = URL(auth.plus("authenticate"))
+        val response = httpClient.get<String>(authUrl) {
             headers {
                 call.request.headers.flattenForEach { key, value ->
                     set(key, value)
@@ -28,13 +28,15 @@ fun Authentication.Configuration.firebase(
             }
         }
 
+        val authResponse = Gson().fromJson(response, AuthResponse::class.java)
+
         val authPrincipal = AuthPrincipal(
-            response.uId,
-            response.name,
-            response.email,
-            response.phone,
-            response.avatar,
-            response.disabled
+            authResponse.uid,
+            authResponse.name,
+            authResponse.email,
+            authResponse.phone,
+            authResponse.avatar,
+            authResponse.disabled
         )
         context.principal(authPrincipal)
     }
@@ -52,7 +54,7 @@ data class AuthPrincipal(
 ) : Principal
 
 data class AuthResponse(
-    val uId: String,
+    val uid: String,
     val name: String?,
     val email: String?,
     val phone: String?,
